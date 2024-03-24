d33f9e2a7773ccef7a9df37723c8e8144cc3e97a
Lyamzin <lyamzin01@mail.ru>
2024-03-24 21:03:51 +1000

    complited

---
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -0,0 +1,43 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Faculty;
+
+import ru.hogwarts.school.service.FacultyService;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/faculty")
+public class FacultyController {
+    private final FacultyService service;
+
+    public FacultyController(FacultyService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public Faculty get(@RequestParam Long id) {
+        return service.get(id);
+    }
+
+    @PostMapping
+    public Faculty add(@RequestBody Faculty faculty) {
+        return service.add(faculty);
+    }
+
+    @DeleteMapping
+    public boolean delete(@RequestParam Long id) {
+        return service.delete(id);
+    }
+
+    @PutMapping
+    public Faculty update(@RequestBody Faculty faculty) {
+        return service.update(faculty);
+    }
+
+    @GetMapping("byColor")
+    public Collection<Faculty> getByColor(@RequestParam String color) {
+        return service.getByColor(color);
+    }
+}
